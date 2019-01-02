64be83e0ffd81a4f110a6d99ec82c47703582b03
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 11:22:40 -0800

    add three string columns, name, image, description. Add references :tag to point foreign key to tags table

--- db/migrate/20190102191113_create_destinations.rb
+++ db/migrate/20190102191113_create_destinations.rb
@@ -1,7 +1,11 @@
 class CreateDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :destinations do |t|
-
+      t.string :name
+      t.string :image
+      t.string :description
+      #add's foreign key pointing to the tags table
+      t.references :tag
       t.timestamps
     end
   end
