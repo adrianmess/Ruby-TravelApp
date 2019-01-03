f1e2280a02477aa0447012bdaf42db5b201894ba
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 16:19:37 -0800

    add show action to Tags controller, retrieves all destinations that to belong to the tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,10 @@ class TagsController < ApplicationController
 		#display a list of all tags by fetching all tags from database and store in variable"
 		@tags = Tag.all
 	end
+
+	def show
+		@tag = Tag.find(params[:id])
+		#retrieves all the destinations that belong to the tag and stores them in @destinations. has_many/belongs_to association lets us query for destinations.
+		@destinations = @tag.destinations
+	end
 end
