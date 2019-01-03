93e6fc313bebcf346b0b780a2a1b824da8a7b2cf
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 17:24:15 -0800

    map requests to /destinations/:id to the Destinations controller's show action, name route destination

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag #name this route "tag"
+  get '/destinations/:id' => 'destinations#show', as: :destination
 end
