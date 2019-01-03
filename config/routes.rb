321aedde76816860af1a40b769770737096d6f78
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 17:54:57 -0800

    add edit and update destination routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag #name this route "tag"
   get '/destinations/:id' => 'destinations#show', as: :destination
+  get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination
+  patch '/destinations/:id' => 'destinations#update'
 end
