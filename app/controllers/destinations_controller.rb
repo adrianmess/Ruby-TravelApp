f0ab09a4365741318ab88ec7cf36a760ddfbb739
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 17:59:40 -0800

    add update action to destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,11 +1,22 @@
 class DestinationsController < ApplicationController
+
 	  def show
 		#use params to find destination by id and save it
   	@destination = Destination.find(params[:id])
 	end
+
 	 def edit
   	@destination = Destination.find(params[:id])
 	end
+
+	def update
+    @destination = Destination.find(params[:id])
+      if @destination.update(destination_params)
+        redirect_to @destination
+      else
+        render 'edit'
+      end
+  	end
 
 	private
   	def destination_params
