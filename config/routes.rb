7f377790fc26933755e63438f3da7b37358327c4
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 16:14:45 -0800

    add action to display a specific tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tags' => 'tags#index'
+  get '/tags/:id' => 'tags#show', as: :tag #name this route "tag"
 end
