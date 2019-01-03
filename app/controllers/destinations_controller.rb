2bd00ecddbef246829478c94d9f6d9ed1406c9c5
Adrian Messado <adrian.messado@gmail.com>
2019-01-02 17:30:43 -0800

    add comment

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,5 +1,6 @@
 class DestinationsController < ApplicationController
-  	def show
+	  def show
+		#use params to find destination by id and save it
   	@destination = Destination.find(params[:id])
 	end
 end
